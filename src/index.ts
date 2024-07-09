707378e76e43639d4de1ee8e370cff0d66d817a6
郑俊鑫 <zhengjunxin.zjx@bytedance.com>
2024-07-09 10:51:46 +0800

    feat: 支持返回附件

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,13 @@
-import { basekit, FieldType, field, FieldComponent, FieldCode, NumberFormatter } from '@lark-opdev/block-basekit-server-api';
-
-const { t } = field;
+import { basekit, FieldType, field, FieldComponent, FieldCode, NumberFormatter, AuthorizationType } from '@lark-opdev/block-basekit-server-api';
 
 basekit.addField({
-  i18n: {
-    messages: {
-      'zh-CN': {
-        attachmentLabel: '请选择附件字段',
-        url: '附件地址',
-        name: '附件名称',
-        size: '附件尺寸',
-      },
-      'en-US': {},
-      'ja-JP': {},
-    }
-  },
   formItems: [
     {
-      key: 'attachments',
-      label: t('attachmentLabel'),
+      key: 'url',
+      label: '请选择多行文本字段',
       component: FieldComponent.FieldSelect,
       props: {
-        supportType: [FieldType.Attachment],
+        supportType: [FieldType.Text]
       },
       validator: {
         required: true,
@@ -30,18 +16,19 @@ basekit.addField({
   ],
   // formItemParams 为运行时传入的字段参数，对应字段配置里的 formItems （如引用的依赖字段）
   execute: async (formItemParams, context) => {
-    const { attachments } = formItemParams;
-    const attachment = attachments?.[0];
-    if (attachment) {
-
+    const { url } = formItemParams;
+    if (url) {
       return {
         code: FieldCode.Success, // 0 表示请求成功
         // data 类型需与下方 resultType 定义一致
         data: {
-          id: attachment.tmp_url ?? '', //  附件临时 url
-          url: attachment.tmp_url ?? '', // 附件临时 url
-          name: attachment?.name, // 附件名称
-          size: attachment?.size, // 附件尺寸
+          files: [
+            {
+              name: "图片1.jpg",
+              content: url[0].link,
+              contentType: "URL"
+            }
+          ]
         },
       };
     }
@@ -50,40 +37,22 @@ basekit.addField({
     };
   },
   resultType: {
-    type: FieldType.Object,
+    type: FieldType.Attachment,
     extra: {
       icon: {
-        light: 'https://lf3-static.bytednsdoc.com/obj/eden-cn/eqgeh7upeubqnulog/chatbot.svg',
+        light: 'https://cdn.pixabay.com/photo/2023/06/11/01/24/flowers-8055013_1280.jpg'
       },
-      properties: [
-        {
-          key: 'id',
-          type: FieldType.Text,
-          title: 'id',
-          hidden: true,
-        },
-        {
-          key: 'url',
-          type: FieldType.Text,
-          title: t('url'),
-          primary: true,
-        },
-        {
-          key: 'name',
-          type: FieldType.Text,
-          title: t('name'),
-        },
-        {
-          key: 'size',
-          type: FieldType.Number,
-          title: t('size'),
-          extra: {
-            formatter: NumberFormatter.DIGITAL_ROUNDED_1, // 保留两位小数
-          },
-        },
-      ],
-    },
+      properties: [{
+        key: "id",
+        type: FieldType.Text,
+        title: 'id',
+      }, {
+        key: "id2",
+        type: FieldType.Text,
+        title: '主属性',
+        primary: true,
+      }]
+    }
   },
 });
-
 export default basekit;
