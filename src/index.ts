1f1f348e1bd5649cc5147866786abe49f1db9a1c
qiuxinping <qiuxinping@bytedance.com>
2025-05-12 15:40:55 +0800

    ts console

--- src/index.ts
+++ src/index.ts
@@ -111,6 +111,7 @@ basekit.addField({
     const { flag, attachments } = formItemParams;
     /** 为方便查看日志，使用此方法替代console.log */
     function debugLog(arg: any) {
+      // @ts-ignore
       console.log(JSON.stringify({
         formItemParams,
         context,
